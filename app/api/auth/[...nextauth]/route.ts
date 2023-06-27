4fd3bc739c9c9aac85c0fcdea28ed667c9185828
Arcades2 <peret.etienne@gmail.com>
2023-06-27 13:48:37 +0200

    Fix bad export in nextauth route definitions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,23 +1,5 @@
-import NextAuth, { AuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
-import { env } from '@/env.mjs';
-
-const prisma = new PrismaClient();
-
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  pages: {
-    signIn: '/signin',
-  },
-};
+import NextAuth from 'next-auth';
+import authOptions from '@/nextauth/authOptions';
 
 const handler = NextAuth(authOptions);
 
---
+++ nextauth/authOptions.ts
@@ -0,0 +1,22 @@
+import type { AuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+import { env } from '@/env.mjs';
+
+const prisma = new PrismaClient();
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  pages: {
+    signIn: '/signin',
+  },
+};
+
+export default authOptions;
