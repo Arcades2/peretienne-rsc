2570a4516799904e9facab1461bd7fbb3eddcabd
Arcades2 <peret.etienne@gmail.com>
2024-02-27 16:27:23 +0100

    Presentation

--- app/(routes)/cv/page.tsx
+++ app/(routes)/cv/page.tsx
@@ -3,7 +3,65 @@ import Image from "next/image";
 export default function CvPage() {
   return (
     <div className="container mx-auto prose dark:prose-invert md:prose-lg print:prose">
-      <h2>Experiences</h2>
+      <h2># Presentation</h2>
+      <p>
+        With three and a half years&apos; experience at{" "}
+        <a href="https://integragen.com/" target="_blank">
+          Integragen
+        </a>{" "}
+        (now{" "}
+        <a href="https://oncodna.com/" target="_blank">
+          OncoDNA
+        </a>
+        ), I have made a significant contribution to projects in the field of
+        biotechnology. I played a key role in the maintenance and enhancement of{" "}
+        <a href="https://integragen.com/mercury" target="_blank">
+          Mercury
+        </a>
+        ,{" "}
+        <a
+          href="https://web.archive.org/web/20221129111314/https://integragen.com/bioinformatics-2/sirius"
+          target="_blank"
+        >
+          Sirius
+        </a>{" "}
+        and{" "}
+        <a href="https://oncodna.com/clinical-oncokdm/" target="_blank">
+          OncoKDM
+        </a>{" "}
+        SaaS, medical tools for oncology interpretation and rare disease genome
+        analysis. In particular, I managed Mercury&apos;s transition to React,
+        helped redesign its Symfony API, and successfully migrated OncoKDM from
+        Angular to React. These experiences have enabled me to develop solid
+        skills in full-stack development.
+      </p>
+
+      <p>
+        Prior to joining Integragen, I worked as a freelancer on a variety of
+        projects including mobile applications, storefronts and custom
+        solutions.
+      </p>
+
+      <p>
+        Currently looking for a challenging professional opportunity, I&apos;m
+        passionate about web development and particularly enjoy the opportunity
+        to interact at all stages of a project. My expertise in React and
+        TypeScript enables me to appreciate type-safe environments, use React
+        meta-framework (Next.js, Remix, ...), take advantage of new features
+        (React Server Components), and libraries (tanstack/query,
+        tanstack/router, tRPC, zod, ...). I&apos;m also proficient in
+        transitioning to more modern technology stacks, with a focus on tailored
+        solutions rather than simply following trends.
+      </p>
+      <p>
+        Attracted by technical challenges and large-scale projects, I&apos;m
+        convinced that it&apos;s in these environments that I can fully express
+        my potential and bring real added value. I&apos;m looking for a dynamic
+        and innovative company, offering a flexible and collaborative working
+        environment, where I can continue to develop and contribute to
+        innovative projects.
+      </p>
+      <h2># Experiences</h2>
       <Experience
         company="OncoDNA / Integragen"
         dates="06/2020 - 02/2024"
@@ -112,7 +170,7 @@ function Experience({
   return (
     <div>
       <div className="flex gap-2 items-center h-[80px]">
-        <h3 className="!m-0"># {company}</h3>
+        <h3 className="!m-0">## {company}</h3>
         {logo && (
           <>
             <span>|</span>
